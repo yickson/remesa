4a9bf0a986089c6b8921a6659507503971887f49
Yickson Ramirez <y.ramirez@redcapital.cl>
2019-12-02 11:33:10 -0300

    Cambio visual de la página principal de Tranfex

--- public/js/app.js
+++ public/js/app.js
@@ -108637,8 +108637,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\tranfex\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\tranfex\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/yicksonramirez/Sites/remesa/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/yicksonramirez/Sites/remesa/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ }),
