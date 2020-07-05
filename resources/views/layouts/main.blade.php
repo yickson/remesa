c0d957c3118ad94d4084e97a244f9b4a0d9f7849
yickson <yicksonr@gmail.com>
2020-07-05 14:16:51 -0400

    Menu en formato movil y arreglos del ignore

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -80,6 +80,7 @@
 <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#ff5e15"/></svg></div>
 
 <script src="{{ asset('js/aos.js') }}"></script>
+<script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
 <script src="{{ asset('js/main.js') }}"></script>
 
 </body>
